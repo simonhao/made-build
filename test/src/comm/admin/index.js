e577027fa5900c479a94935f7d19bab972d72aff
IetnHao <dreamschool@163.com>
2015-12-20 20:33:29 +0800

    change test case

--- test/src/comm/admin/index.js
+++ test/src/comm/admin/index.js
@@ -10,15 +10,12 @@ var MadeModule = require('made-module');
 var inherits   = require('inherits');
 
 var AdminFrame = require('comm/admin/frame');
-var AdminNavbar = require('comm/admin/navbar');
 
 function Module(options, instance){
   MadeModule.call(this, __module_id, instance);
 
   this.admin_frame = new AdminFrame({}, __instance(''));
 
-  this.top_navbar = new AdminNavbar({}, __instance('top'));
-  this.bottom_navbar = new AdminNavbar({}, __instance('bottom'));
 }
 
 inherits(Module, MadeModule);
